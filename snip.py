e757542adac8a72e5aa5a6a71fd520da75561c8e
FRex <blazej.roszkowski@gmail.com>
2024-11-27 08:25:18 +0100

    clean up and comment out

--- snip.py
+++ snip.py
@@ -5,30 +5,32 @@ import sys
 import os
 
 
+PIPE = subprocess.PIPE  # shortcut to make multiline Popen call a one liner
+
+
 def main():
     """Main function."""
     snipdir = os.path.join(os.path.dirname(__file__), "files")
     files = sorted(os.listdir(snipdir))  # sort files alphabetically
 
-    # NOTE: repr is to handle \\ on windows
-    # TODO: do it in a better way
+    # NOTE: repr is to handle \\ on windows when given to bat
+    # TODO: find a better way to do it in a crossplatform safe way?
     args = ["fzf", f"--preview=bat {repr(snipdir)}/{{}}", "--tac"]
-    p = subprocess.Popen(
-        args,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        encoding="UTF-8",
-    )
+    with subprocess.Popen(args, stdin=PIPE, stdout=PIPE, encoding="UTF-8") as proc:
+        chosen = proc.communicate("\n".join(files))[0].strip()
+        if proc.returncode != 0:
+            print(f"return code is {proc.returncode} - doing nothing!", file=sys.stderr)
+            sys.exit(proc.returncode)  # forward the return code
 
-    out = p.communicate("\n".join(files))[0]
+    # NOTE: even with spaces and backslashes, path with quotes around it works in cmd, ps and bash
+    # NOTE: printing all diagnostics to stderr, only file goes to the stdout
+    print(f'cd "{snipdir}"', file=sys.stderr)
 
-    if p.returncode != 0:
-        print(f"return code is {p.returncode} - doing nothing", file=sys.stderr)
-        return
-
-    with open(os.path.join(snipdir, out.strip()), "rb") as fptr:
+    # dump entire file to stdout as binary to not convert newlines on windows
+    # the flushing before and after is just in case (probably not needed)
+    with open(os.path.join(snipdir, chosen), "rb") as file:
         sys.stdout.flush()
-        sys.stdout.buffer.write(fptr.read())
+        sys.stdout.buffer.write(file.read())
         sys.stdout.flush()
 
 
